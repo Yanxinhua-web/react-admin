596fbe2d6ba4bd363ff29ee58c85c1530c5c9606
Yanhaha <2990497363@qq.com>
2021-01-10 14:27:58 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
+      <Button>aaa</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
